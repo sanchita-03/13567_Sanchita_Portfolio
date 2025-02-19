85aa209ffa98f3549ce04d9007e473ed7d9673f3
sanchita-03 <137096047+sanchita-03@users.noreply.github.com>
2025-02-19 16:07:08 +0530

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,73 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+document.getElementById("contactUsForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form from refreshing the page
+    alert("Your message has been sent successfully!");
+    this.reset(); // Clear the form after submission
+});
+
+
+
+// certifates slide gallery
+$(document).ready(function(){
+  $(".roll-container img").click(function(){
+      $("#modalImg").attr("src", $(this).attr("src"));
+      $("#myModal").fadeIn();
+  });
+  $(".close, #myModal").click(function(){
+      $("#myModal").fadeOut();
+  });
+});
